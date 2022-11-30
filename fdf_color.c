fbd7ef7d22025e52ef94d51ff55560fb53ea3b24
Javier Garcia-arango vazquez-prada <javigarc@c1r3s4.42urduliz.com>
2022-11-30 15:32:39 +0100

    header

--- fdf_color.c
+++ fdf_color.c
@@ -5,12 +5,10 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: javigarc <javigarc@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/13 15:38:38 by javigarc          #+#    #+#             */
-/*   Updated: 2022/11/25 13:56:44 by javigarc         ###   ########.fr       */
+/*   Created: 2022/11/30 15:28:25 by javigarc          #+#    #+#             */
+/*   Updated: 2022/11/30 15:30:51 by javigarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "fdf.h"
 
 double	ft_percent(int start, int end, int current)
 {
